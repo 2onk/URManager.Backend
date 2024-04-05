1a0d396f5471e26818e0fb3f3ea2b6e462849434
David Sadek <dasa@universal-robots.com>
2024-04-05 07:25:56 +0200

    Add project files.

---
+++ Model/ItemLogger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace URManager.Backend.Model
+{
+    public class ItemLogger : ObservableCollection<string>
+    {
+        public ItemLogger()
+        {
+
+        }
+        /// <summary>
+        /// Add any Information as string to the ObservableCollection
+        /// </summary>
+        /// <param name="item"></param>
+        public ItemLogger(string item)
+        {
+            this.Add(item);
+        }
+    }
+}
---
+++ Model/Robot.cs
@@ -0,0 +1,9 @@
+﻿namespace URManager.Backend.Model
+{
+    public class Robot
+    {
+        public int Id { get; set; }
+        public string? RobotName { get; set; }
+        public string? IP { get; set; }
+    }
+}
