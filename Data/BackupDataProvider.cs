b08043b810daa9f32cfbeb3be441d1370e0b0791
David Sadek <dasa@universal-robots.com>
2024-04-10 10:33:04 +0200

    introduced backupintervall class for combobox, show combobox list in UI

---
+++ Data/BackupDataProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using URManager.Backend.Model;
+
+namespace URManager.Backend.Data
+{
+    public interface IBackupDataProvider
+    {
+        IEnumerable<BackupIntervall> GetAll();
+    }
+
+    public class BackupDataProvider : IBackupDataProvider
+    {
+        public IEnumerable<BackupIntervall> GetAll()
+        {
+            return new List<BackupIntervall>
+            {
+                new BackupIntervall("1 day", 1),
+                new BackupIntervall("7 days", 7),
+                new BackupIntervall("14 days", 14),
+                new BackupIntervall("31 days", 31),
+            };
+        }
+    }
+}
---
+++ Model/BackupIntervall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using URManager.Backend.ViewModel;
+
+namespace URManager.Backend.Model
+{
+    public class BackupIntervall
+    {
+
+        public string Name { get; }
+        public int Intervall { get; }
+
+        public BackupIntervall(string name, int intervall)
+        {
+            Name = name;
+            Intervall = intervall;
+        }
+    }
+}
