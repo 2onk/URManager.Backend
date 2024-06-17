3299970ea01a2d853c152ce24a1e5c74b86c8cbc
David Sadek <dasa@universal-robots.com>
2024-06-17 14:43:53 +0200

    added checkbox to choose robots for backup

--- Model/Robot.cs
+++ Model/Robot.cs
@@ -2,15 +2,17 @@
 {
     public class Robot
     {
-        public Robot(int id, string robotName, string ip) 
+        public Robot(int id, string robotName, string ip , bool backup=true) 
         {
             Id = id;
             RobotName = robotName;
             IP = ip;
+            Backup = backup;
         }
 
         public int Id { get; set; }
         public string RobotName { get; set; }
         public string IP { get; set; }
+        public bool Backup {  get; set; }
     }
 }
