d34dcba2133891589d1d3c79e97c929582e31dfd
David Sadek <dasa@universal-robots.com>
2024-06-25 10:01:42 +0200

    changed settingsviewmodel to property, changed toggle button as mode two way

--- Model/Robot.cs
+++ Model/Robot.cs
@@ -2,17 +2,19 @@
 {
     public class Robot
     {
-        public Robot(int id, string robotName, string ip , bool backup=true) 
+        public Robot(int id, string robotName, string ip , bool backup=true, bool update=false) 
         {
             Id = id;
             RobotName = robotName;
             IP = ip;
             Backup = backup;
+            Update = update;
         }
 
         public int Id { get; set; }
         public string RobotName { get; set; }
         public string IP { get; set; }
         public bool Backup {  get; set; }
+        public bool Update { get; set; }
     }
 }
