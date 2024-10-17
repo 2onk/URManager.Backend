839cc5a1b1ae1e9e6a06f7e45d1643cdb71a270e
David Sadek <dasa@universal-robots.com>
2024-10-17 09:50:01 +0200

    unocommented report to dasa button

--- Tracker/User.cs
+++ Tracker/User.cs
@@ -45,40 +45,6 @@ namespace URManager.Backend.Tracker
             }
         }
 
-        //public static async Task TrackEvent()
-        //{
-        //    var clientId = GenerateClientId();
-
-        //    string serverUrl = "https://urmanager.de/index.php?option=com_eventtracker";
-        //    using (HttpClient client = new HttpClient())
-        //    {
-        //        var values = new Dictionary<string, string>
-        //        {
-        //        { "uniqueId", clientId },
-        //        { "timestamp", DateTime.Now.ToString("o") }
-        //    };
-
-        //        try
-        //        {
-        //            var content = new FormUrlEncodedContent(values);
-        //            var response = await client.PostAsync(serverUrl, content);
-
-        //            if (response.IsSuccessStatusCode)
-        //            {
-        //                Console.WriteLine("Server notified successfully.");
-        //            }
-        //            else
-        //            {
-        //                Console.WriteLine("Failed to notify server.");
-        //            }
-        //        }
-        //        catch (Exception)
-        //        {
-        //            Console.WriteLine("Failed to notify server.");
-        //        }
-
-        //    }
-        //}
         public static string GenerateClientId()
         {
             string result = "";
