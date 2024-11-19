464d23db0f8a8b969e58c267b75740a3f60f5190
David Sadek <dasa@universal-robots.com>
2024-11-19 14:06:03 +0100

    added succes window

--- FlexibleEthernetIp/FlexibleEthernetIpEdsGenerator.cs
+++ FlexibleEthernetIp/FlexibleEthernetIpEdsGenerator.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using URManager.Backend.Model;
 
 namespace URManager.Backend.FlexibleEthernetIp
